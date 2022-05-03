7e5b4b33afc8b79141cb7cbc8b8036d916ad5e52
NT <nturner@zetta.ai>
2022-05-02 23:48:05 -0400

    feat: faster loading function from Eleuther

---
+++ gptomics/transformersio.py
@@ -0,0 +1,28 @@
+"""Some utility functions for working with HuggingFace transformers models."""
+import torch
+from transformers import AutoModelForCausalLM
+
+
+# A helper function to load models more quickly. Taken from Eleuther discord (#gpt-j)
+def no_init(loading_code):
+    def dummy(self):
+        return
+
+    modules = [torch.nn.Linear, torch.nn.Embedding, torch.nn.LayerNorm]
+    original = {}
+    for mod in modules:
+        original[mod] = mod.reset_parameters
+        mod.reset_parameters = dummy
+
+    result = loading_code()
+    for mod in modules:
+        mod.reset_parameters = original[mod]
+
+    return result
+
+
+def load_model(modelname: str) -> AutoModelForCausalLM:
+    def load():
+        return AutoModelForCausalLM.from_pretrained(modelname, low_cpu_mem_usage=True)
+
+    return no_init(load)
