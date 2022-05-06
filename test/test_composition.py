b78077739a9fe450271ae5c9fbf9a20befbc53af
NT <nturner@zetta.ai>
2022-05-06 12:31:15 -0400

    test: removemean

---
+++ test/test_composition.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from gptomics import composition as comp
+
+
+def test_removemean():
+    assert np.all(np.isclose(comp.removemean(np.ones((10, 10))), 0,))
+    assert np.all(
+        np.isclose(comp.removemean(np.ones((10, 10)), method="matrix multiply"), 0,)
+    )
