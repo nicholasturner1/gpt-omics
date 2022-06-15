b8eb2381812b0b2b807c7618ef433fdc699c8fb3
NT <nturner@zetta.ai>
2022-06-15 16:15:35 -0400

    feat: weight matrix singular values

---
+++ scripts/compute_weight_singular_values.py
@@ -0,0 +1,73 @@
+"""Computes all of the singular values for the weight matrices across a model.
+
+Writes a npz file with the singular values mapped to each tensor name.
+"""
+import time
+import argparse
+
+import numpy as np
+from tqdm import tqdm
+
+from gptomics import torchio
+from gptomics.svd import SVD
+
+
+def main(
+    modelfilename: str,
+    outputfilename: str,
+    verbose: bool = True,
+) -> None:
+    if verbose:
+        print("Starting computation")
+        begin = time.time()
+
+    singularvalues = weight_singular_values(modelfilename)
+
+    if verbose:
+        end = time.time()
+        print(f"Computation complete in {end-begin:.3f}s")
+
+    writenpz(singularvalues, outputfilename)
+
+
+def weight_singular_values(modelfilename: str) -> dict[str, np.ndarray]:
+    """Computes the singular values for each tensor with "weight" in its name."""
+
+    tensor_names = torchio.read_tensor_names(modelfilename)
+
+    svs = dict()
+
+    filtered_names = list(filter(lambda n: "weight" in n, tensor_names))
+
+    for tn in tqdm(filtered_names):
+        tensor = torchio.read_tensor(modelfilename, tn).numpy()
+        svs[tn] = SVD.frommatrix(tensor).S
+
+    return svs
+
+
+def writenpz(singularvalues: dict, outputfilename: str) -> None:
+    """Writes the singular values to a file.
+
+    Args:
+        singularvalues: A dict mapping tensor names to singular value (1D) arrays.
+        outputfilename: The filename to use when writing the file.
+    """
+    np.savez_compressed(outputfilename, **singularvalues)
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument(
+        "modelfilename", type=str, help="path to the pytorch_model.bin file"
+    )
+    ap.add_argument("outputfilename", type=str, help="output filename")
+    ap.add_argument(
+        "--quiet",
+        dest="verbose",
+        action="store_false",
+        help="Do not print progress messages",
+    )
+
+    main(**vars(ap.parse_args()))
