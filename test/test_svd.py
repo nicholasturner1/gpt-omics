609f2cfab59c829f2faf26452ef7b593cb05c512
NT <nturner@zetta.ai>
2022-10-20 19:12:42 -0400

    test: numpy -> torch

--- test/test_svd.py
+++ test/test_svd.py
@@ -3,20 +3,20 @@ from __future__ import annotations
 import random
 from typing import Optional
 
-import numpy as np
+import torch
 
 from gptomics.svd import SVD
 
 
 def check_svd(seed: Optional[int]):
     if seed is not None:
-        np.random.seed(seed)
+        torch.manual_seed(seed)
 
-    M1 = np.random.rand(10, 10)
-    M2 = np.random.rand(10, 10)
+    M1 = torch.rand(10, 10)
+    M2 = torch.rand(10, 10)
 
-    assert np.all(
-        np.isclose(SVD.frommatrices(M1, M2).full(), M1 @ M2)
+    assert torch.all(
+        torch.isclose(SVD.frommatrices(M1, M2).full(), M1 @ M2)
     ), f"fails for seed {seed}"
 
 
